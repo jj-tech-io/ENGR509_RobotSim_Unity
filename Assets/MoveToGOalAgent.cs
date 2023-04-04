7c52517fc2a5a7b2a944352fece44c7cdff9a39a
Joel Johnson <60250054+jj-tech-io@users.noreply.github.com>
2023-04-04 00:45:11 -0700

    before MDP attempt

---
+++ Assets/MoveToGOalAgent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class MoveToGoalAgent : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        // base.OnActionReceived(actions);
+        Debug.Log(actions.DiscreteActions[0]);
+    }
+
+}
